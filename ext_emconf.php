2e268a8b670ef0ca376511a27b245c4a4c5349fc
Florian Rival <florian.rival@oktopuce.fr>
2020-03-13 17:50:26 +0100

    Update description

--- ext_emconf.php
+++ ext_emconf.php
@@ -6,7 +6,7 @@
 
 $EM_CONF[$_EXTKEY] = [
     'title' => 'Site generator / tree model duplicator',
-    'description' => 'Site generator wizard : used to generate mini-website or duplicate tree model and affect groups, create folder and so on',
+    'description' => 'Site generator wizard used to generate mini-website or duplicate tree model, it will automatically create associated BE/FE groups, create directories with associated files mount, add domain name, update Typoscript configuration (folders/pages ID and TCEMAIN.clearCacheCmd), update slugs.',
     'category' => 'services',
     'author' => 'Florian Rival',
     'author_email' => 'florian.typo3@oktopuce.fr',
