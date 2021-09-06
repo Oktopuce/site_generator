526bf72b4725c7b6bb7466cf6b7fa0d294917aa7
Florian <florian.tyo3@oktopuce.fr>
2021-09-06 17:47:52 +0200

    [BUGFIX] Correct bug if there is no TS on home page

--- Classes/Wizard/StateUpdatePageTs.php
+++ Classes/Wizard/StateUpdatePageTs.php
@@ -1,15 +1,17 @@
 <?php
 
-namespace Oktopuce\SiteGenerator\Wizard;
+declare(strict_types=1);
 
-/* * *
+/*
  *
  * This file is part of the "Site Generator" Extension for TYPO3 CMS.
  *
  * For the full copyright and license information, please read the
  * LICENSE.txt file that was distributed with this source code.
  *
- * * */
+ */
+
+namespace Oktopuce\SiteGenerator\Wizard;
 
 use TYPO3\CMS\Core\Utility\GeneralUtility;
 use TYPO3\CMS\Core\Log\LogLevel;
@@ -52,45 +54,47 @@ class StateUpdatePageTs extends StateBase implements SiteGeneratorStateInterface
             // Get record data
             $origRow = BackendUtility::getRecord('pages', $pageId);
 
-            $newsTSConfig = '';
-            $line = strtok($origRow['TSconfig'], PHP_EOL);
-            $matches = null;
+            if ($origRow['TSconfig']) {
+                $newsTSConfig = '';
+                $line = strtok($origRow['TSconfig'], PHP_EOL);
+                $matches = null;
 
-            while ($line !== false) {
-                // If clearCacheCmd found, update it with new uids
-                if (preg_match('/(clearCacheCmd)(\s)*=(.*)/', $line, $matches)) {
-                    $commands = GeneralUtility::trimExplode(',', $matches[3], true);
-                    $clearCacheCommands = array_unique($commands);
+                while ($line !== false) {
+                    // If clearCacheCmd found, update it with new uids
+                    if (preg_match('/(clearCacheCmd)(\s)*=(.*)/', $line, $matches)) {
+                        $commands = GeneralUtility::trimExplode(',', $matches[3], true);
+                        $clearCacheCommands = array_unique($commands);
 
-                    foreach ($clearCacheCommands as &$clearCacheCommand) {
-                        $clearCacheCommand = ($siteData->getMappingArrayMerge()[$clearCacheCommand] ?? '');
+                        foreach ($clearCacheCommands as &$clearCacheCommand) {
+                            $clearCacheCommand = ($siteData->getMappingArrayMerge()[$clearCacheCommand] ?? '');
+                        }
+
+                        $newsTSConfig .= str_replace($matches[0], $matches[1] . ' = ' . implode(',', $clearCacheCommands), $line) . PHP_EOL;
+                        $update = true;
+                    } else {
+                        $newsTSConfig .= $line . PHP_EOL;
                     }
-
-                    $newsTSConfig .= str_replace($matches[0], $matches[1] . ' = ' . implode(',', $clearCacheCommands), $line) . PHP_EOL;
-                    $update = true;
+                    $line = strtok(PHP_EOL);
                 }
-                else {
-                    $newsTSConfig .= $line . PHP_EOL;
+
+                if ($update) {
+                    // Set the data to be saved
+                    $recData = [];
+                    $recData['pages'][$pageId]['TSconfig'] = $newsTSConfig;
+                    // Create new  tce-object
+                    $tce = GeneralUtility::makeInstance(DataHandler::class);
+                    $tce->start($recData, []);
+                    $tce->process_datamap();
+
+                    $pagesUpdated[] = $pageId;
                 }
-                $line = strtok(PHP_EOL);
-            }
-
-            if ($update) {
-                // Set the data to be saved
-                $recData = [];
-                $recData['pages'][$pageId]['TSconfig'] = $newsTSConfig;
-                // Create new  tce-object
-                $tce = GeneralUtility::makeInstance(DataHandler::class);
-                $tce->start($recData, []);
-                $tce->process_datamap();
-
-                $pagesUpdated[] = $pageId;
             }
         }
+
         if (!empty($pagesUpdated)) {
             $this->log(LogLevel::NOTICE, 'Update page TSConfig : TCEMAIN.clearCacheCmd done for pages : ' . implode(',', $pagesUpdated));
+            // @extensionScannerIgnoreLine
             $siteData->addMessage($this->translate('generate.success.updatePageTS', [implode(',', $pagesUpdated)]));
         }
     }
-
 }
