a22ff1c6d8815858fa6589abe283cddf600a9b38
Florian <florian.tyo3@oktopuce.fr>
2021-09-23 19:16:56 +0200

    [TASK] Set unit test

---
+++ Tests/Unit/Dto/BaseDtoTest.php
@@ -0,0 +1,37 @@
+<?php
+declare(strict_types=1);
+
+namespace Oktopuce\SiteGenerator\Tests\Unit\Dto;
+
+use PHPunit\Framework\TestCase;
+use Oktopuce\SiteGenerator\Dto\BaseDto;
+
+/**
+ * Tests for base DTO
+ *
+ */
+class BaseDtoTest extends TestCase
+{
+    /**
+     * @var BaseDto
+     */
+    protected $baseDto = null;
+
+    protected function setUp(): void
+    {
+        $this->baseDto = new BaseDto();
+    }
+
+    /**
+     * @test
+     */
+    public function testGetTitleSanitize()
+    {
+        $title = 'é_jfez6(43)0à÷¡÷©ëd©';
+        $this->baseDto->setTitle($title);
+
+        // Test title sanitation for folder creation
+        $this->assertEquals($this->baseDto->getTitleSanitize(), "-jfez6-43-0-d-");
+    }
+
+}
