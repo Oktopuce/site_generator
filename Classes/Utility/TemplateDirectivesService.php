3f8c0ee60acc6bec5920a61f61bc282235752679
Florian Rival <florian.typo3@oktopuce.fr>
2022-02-15 10:51:34 +0100

    [FEATURE] Manage directives for constants mapping customization in home page template

---
+++ Classes/Utility/TemplateDirectivesService.php
@@ -0,0 +1,98 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ *
+ * This file is part of the "Site Generator" Extension for TYPO3 CMS.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ */
+
+namespace Oktopuce\SiteGenerator\Utility;
+
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+
+/**
+ * ExtendedTemplateService
+ */
+class TemplateDirectivesService
+{
+    /** @var array */
+    protected array $directives = [];
+
+    /**
+     * Search for directives in constant comment, sample :
+     * # ext=SiteGenerator; table=tt_content; action=mapInString; ignoreUids=777,888
+     *
+     * Available directives :
+     * ext=SiteGenerator : mandatory
+     * table=(page | tt_content | tx_mytable) : the table used for the mapping
+     * action=(mapInString | mapInList | exclude)
+     * parameters=my_params : parameters for a custom directive
+     * ignoreUids=777,888 : list of uids to ignore
+     *
+     * @param string $lineFromConstant The constant line
+     * @return void
+     */
+    public function lookForDirectives(string $lineFromConstant): void
+    {
+        // Remove all spaces and line feed from input line
+        $inputLine = trim(str_replace(' ', '', $lineFromConstant));
+        $directivesPattern = '(?:;(action|table|ignoreUids|parameters)=([^;=]+))?';
+        $regEx = "/^(?:#ext=SiteGenerator)$directivesPattern$directivesPattern$directivesPattern$directivesPattern;?/";
+        $matchDirective = [];
+        $this->directives = [];
+
+        if (preg_match($regEx, $inputLine, $matchDirective)) {
+            for ($i = 1, $iMax = count($matchDirective); $i < $iMax; $i += 2) {
+                if (isset($matchDirective[$i], $matchDirective[$i + 1])) {
+                    $this->directives[$matchDirective[$i]] = $matchDirective[$i + 1];
+                }
+            }
+        }
+    }
+
+    /**
+     * Return the name of the action in directives
+     * @param string $default Default value
+     * @return string
+     */
+    public function getAction(string $default = ''): string
+    {
+        return ($this->directives['action'] ?? $default);
+    }
+
+    /**
+     * Get the parameters, for custom extensions, can be used with action
+     * # ext=SiteGenerator; action=customAction; parameters=myparams
+     *
+     * @return string
+     */
+    public function getParameters(): string
+    {
+        return ($this->directives['parameters'] ?? '');
+    }
+
+    /**
+     * Return the name of the table in directives
+     * @param string $default Default value
+     * @return string
+     */
+    public function getTable(string $default = ''): string
+    {
+        return ($this->directives['table'] ?? $default);
+    }
+
+    /**
+     * Return the list of uids to ignore
+     * @param string $default Default values - comma separated (sample : 12,54)
+     * @return string
+     */
+    public function getIgnoreUids(string $default = ''): string
+    {
+        return ($this->directives['ignoreUids'] ?? $default);
+    }
+}
--- Classes/Wizard/StateUpdateTemplateHP.php
+++ Classes/Wizard/StateUpdateTemplateHP.php
@@ -13,17 +13,48 @@ declare(strict_types=1);
 
 namespace Oktopuce\SiteGenerator\Wizard;
 
+use Oktopuce\SiteGenerator\Utility\TemplateDirectivesService;
+use Psr\EventDispatcher\EventDispatcherInterface;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
-use TYPO3\CMS\Core\Log\LogLevel;
+use Psr\Log\LogLevel;
 use TYPO3\CMS\Core\DataHandling\DataHandler;
 use Oktopuce\SiteGenerator\Dto\BaseDto;
 use Oktopuce\SiteGenerator\Utility\ExtendedTemplateService;
+use Oktopuce\SiteGenerator\Wizard\Event\UpdateTemplateHPEvent;
 
 /**
  * StateUpdateTemplate
  */
 class StateUpdateTemplateHP extends StateBase implements SiteGeneratorStateInterface
 {
+    /** @var TemplateDirectivesService */
+    protected TemplateDirectivesService $templateDirectivesService;
+
+    /**
+     * @var EventDispatcherInterface
+     */
+    protected $eventDispatcher;
+
+    /**
+     * @var ExtendedTemplateService
+     */
+    protected $templateService;
+
+    /**
+     * @param TemplateDirectivesService $templateDirectivesService
+     * @param EventDispatcherInterface $eventDispatcher
+     * @param ExtendedTemplateService $templateService
+     */
+    public function __construct(
+        TemplateDirectivesService $templateDirectivesService,
+        EventDispatcherInterface $eventDispatcher,
+        ExtendedTemplateService $templateService
+    ) {
+        parent::__construct();
+        $this->templateDirectivesService = $templateDirectivesService;
+        $this->eventDispatcher = $eventDispatcher;
+        $this->templateService = $templateService;
+    }
 
     /**
      * Update site template with the new uids
@@ -45,37 +76,128 @@ class StateUpdateTemplateHP extends StateBase implements SiteGeneratorStateInter
      */
     protected function updateTemplate(BaseDto $siteData): void
     {
-        /** @var ExtendedTemplateService $templateService */
-        $templateService = GeneralUtility::makeInstance(ExtendedTemplateService::class);
         // Get the row of the first VISIBLE template of the page. where clause like the frontend.
-        $templateRow = $templateService->ext_getFirstTemplate($siteData->getHpPid());
+        $templateRow = $this->templateService->ext_getFirstTemplate($siteData->getHpPid());
 
         if (!empty($templateRow)) {
-            $templateService->ext_regObjectPositions($templateRow['constants']);
-            $objReg = $templateService->getObjReg();
+            $this->templateService->ext_regObjectPositions($templateRow['constants']);
+            $objReg = $this->templateService->getObjReg();
 
             foreach ($objReg as $key => $rawP) {
-                $value = GeneralUtility::trimExplode('=', $templateService->raw[$rawP]);
-                foreach ($siteData->getMappingArrayMerge('pages') as $modelPid => $sitePid) {
-                    if ($modelPid === (int)$value[1]) {
-                        $templateService->ext_putValueInConf($key, $sitePid);
-                    }
+                // Looking for directives in comments
+                $this->templateDirectivesService->lookForDirectives(($rawP > 0 ? $this->templateService->raw[$rawP - 1] : ''));
+                $table = $this->templateDirectivesService->getTable('pages');
+
+                $value = GeneralUtility::trimExplode('=', $this->templateService->raw[$rawP]);
+
+                $uidsToExclude = GeneralUtility::trimExplode(',',
+                    $this->templateDirectivesService->getIgnoreUids(), true);
+                $filteredMapping = $mapping = $siteData->getMappingArrayMerge($table);
+
+                // Manage uids to exclude
+                if (!empty($uidsToExclude)) {
+                    $filteredMapping = array_filter($mapping, static function ($key) use ($uidsToExclude) {
+                        return !in_array((string)$key, $uidsToExclude, true);
+                    }, ARRAY_FILTER_USE_KEY);
+                }
+
+                $action = $this->templateDirectivesService->getAction('mapInList');
+                $updatedValue = '';
+
+                switch ($action) {
+                    case 'mapInList' :
+                        $updatedValue = $this->mapInList($value[1], $filteredMapping);
+                        break;
+                    case 'mapInString' :
+                        $updatedValue = $this->mapInString($value[1], $filteredMapping);
+                        break;
+                    case 'exclude' :
+                        // Exclude all line
+                        break;
+                    default :
+                        // Call custom action if there is one
+                        $parameters = $this->templateDirectivesService->getParameters();
+                        $event = $this->eventDispatcher->dispatch(new UpdateTemplateHPEvent($action, $parameters, $value[1], $filteredMapping, $this->templateDirectivesService));
+                        $updatedValue = $event->getUpdatedValue();
+                        break;
+                }
+
+                if (!empty($updatedValue)) {
+                    $this->templateService->ext_putValueInConf($key, $updatedValue);
                 }
             }
 
-            if ($templateService->changed) {
+            if ($this->templateService->changed) {
                 // Set the data to be saved
                 $recData = [];
                 $saveId = $templateRow['uid'];
-                $recData['sys_template'][$saveId]['constants'] = implode(LF, $templateService->raw);
+                $recData['sys_template'][$saveId]['constants'] = implode(LF, $this->templateService->raw);
                 // Create new  tce-object
                 $tce = GeneralUtility::makeInstance(DataHandler::class);
                 $tce->start($recData, []);
                 $tce->process_datamap();
 
-                $this->log(LogLevel::NOTICE, 'Update home page template with new pid done');
+                // @extensionScannerIgnoreLine
+                $siteData->addMessage($this->translate('generate.success.templateHpUpdated'));
+                $this->log(LogLevel::NOTICE, 'Update home page template with new uids done');
             }
         }
     }
 
+    /**
+     * Update constant in list
+     *
+     * @param string $value
+     * @param array $filteredMapping
+     * @return string Empty string or value updated
+     */
+    protected function mapInList(
+        string $value,
+        array $filteredMapping
+    ): string {
+        // Check if the value in constant is a list of int - 78,125,98 - or just an int
+        if (preg_match('/^\d+(?:,\d+)*$/', $value)) {
+            $updateConstant = false;
+
+            $listOfInt = GeneralUtility::trimExplode(',', $value, true);
+
+            // Set new uid for constants
+            array_walk($listOfInt,
+                static function (&$constantValue) use ($filteredMapping, &$updateConstant) {
+                    if (isset($filteredMapping[(int)$constantValue])) {
+                        $constantValue = $filteredMapping[(int)$constantValue];
+                        $updateConstant = true;
+                    }
+                });
+
+            if ($updateConstant) {
+                return implode(',', $listOfInt);
+            }
+        }
+        return ('');
+    }
+
+    /**
+     * Update constants in string
+     *
+     * @param string $value
+     * @param array $filteredMapping
+     * @return string Empty string or value updated
+     */
+    protected function mapInString(
+        string $value,
+        array $filteredMapping
+    ): string {
+        $updateConstant = false;
+        $count = 0;
+
+        foreach ($filteredMapping as $modelUid => $siteUid) {
+            $value = str_replace((string)$modelUid, (string)$siteUid, $value, $count);
+            $updateConstant = ($updateConstant || $count > 0);
+        }
+        if ($updateConstant) {
+            return($value);
+        }
+        return ('');
+    }
 }
